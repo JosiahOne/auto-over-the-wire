e295c2c0d44a8e346e4a6bcb51c494197d2ea14d
Josiah Bruner <josiah@programmer.net>
2017-06-10 14:21:31 -0400

    Solve level 2 and 3 of Bandit

---
+++ solveLevel2.sh
@@ -0,0 +1,1 @@
+sshpass -f passwords/level2Pass.txt ssh bandit2@bandit.labs.overthewire.org -p 2220 "cat 'spaces in this filename'" > passwords/level3Pass.txt
---
+++ solveLevel3.sh
@@ -0,0 +1,1 @@
+sshpass -f passwords/level3Pass.txt ssh bandit3@bandit.labs.overthewire.org -p 2220 "cat inhere/.hidden" > passwords/level4Pass.txt
